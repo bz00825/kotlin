a79c1516fe9465b7d1024b50e1b21e65283733b1
Banzragch BZ Ganchimeg <BZ@Banzragchs-MBP.lan>
2019-03-01 09:40:48 +0800

    First commit

---
+++ AD-3-1.kt
@@ -0,0 +1,31 @@
+/**
+ * Banzragch Ganchimeg(BZ), 2016139
+ * March 1st, 2019
+ */
+
+fun main() {
+    var a = 5
+	var b = 2
+    var english = 90
+	var history = 80
+	var music = 80
+    println("2.-------------------------------")
+
+    println("I Love Coding")
+
+    println("3.-------------------------------")
+
+	println(a * b)
+
+    println("4.-------------------------------")
+
+	println("Your Grade:")
+	println("English:"+english)
+	println("History: "+history)
+	println("Music: "+music)
+
+    println("Total: "+ (english + history + music))
+    println("Average: "+((english + history + music)/3))
+
+
+}
