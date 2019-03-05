7c05f5f49f9cd1379d396a95e66e2de24cf9c61c
Banzragch BZ Ganchimeg <BZ@Banzragchs-MacBook-Pro.local>
2019-03-05 13:28:38 +0800

    First commmit

---
+++ AP-2-2.kt
@@ -0,0 +1,17 @@
+/**
+ * Banzragch Ganchimeg(BZ), 2016139
+ * March 5th, 2019
+ */
+
+fun main(args: Array<String>) {
+    println("Finding Bigger Number")
+    println("x = 52 ,y = 87")
+  var x = 52
+  var y = 87
+
+  if (x >= y)
+    println("x $x is more")
+  else
+    println("y $y is more")
+
+}
---
+++ AP-2-3.kt
@@ -0,0 +1,19 @@
+/**
+ * Banzragch Ganchimeg(BZ), 2016139
+ * March 5th, 2019
+ */
+
+fun main(args: Array<String>) {
+    println("Finding Bigger Number")
+    println("x = 52 ,y = 87 ,z = 38")
+  var x = 52
+  var y = 87
+  var z = 38
+
+  if(x >= y && x >= z)
+      	println("$x is largest number")
+      else if(y >= x && y >= z)
+      	println("$y is largest number")
+      else
+      	println("$z is largest number")
+}
